ffed574b09e2e1a46f7afc66da13ccb00b53d67c
Acellam Guy <acellam.guy@gmail.com>
2025-03-27 14:05:58 +0300

    feat(loans): add rest to request for loan

--- src/main/java/com/acellam/lms/loans/loan/LoansController.java
+++ src/main/java/com/acellam/lms/loans/loan/LoansController.java
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.acellam.lms.loans.loan.dtos.LoanCheckStatusDto;
+import com.acellam.lms.loans.loan.dtos.LoanClientRequestDto;
 import com.acellam.lms.loans.loan.dtos.LoanResponseDto;
 import com.acellam.lms.loans.loan.service.LoanService;
 
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RequestMapping("api/v1/loans")
 @RestController
@@ -25,5 +28,10 @@ public class LoansController {
             @Valid @PathVariable("customerNumber") LoanCheckStatusDto loanCheckStatusDto) {
 
         return this.loanService.checkLoanStatus(loanCheckStatusDto);
+    }
+
+    @PostMapping
+    public LoanResponseDto createLoan(@Valid @RequestBody LoanClientRequestDto loanClientRequestDto) {
+        return this.loanService.createLoan(loanClientRequestDto);
     }
 }
